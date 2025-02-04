25c077ca403eb9e4948099c398eb50bc579a179a
MCredbear <2363764471@qq.com>
2025-02-04 12:49:54 +0800

    Update android build template

--- android/app/src/main/kotlin/com/example/flutter_hentai_viewer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_hentai_viewer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/moe/redbear/flutter_hentai_viewer/MainActivity.kt
@@ -0,0 +1,5 @@
+package moe.redbear.flutter_hentai_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
